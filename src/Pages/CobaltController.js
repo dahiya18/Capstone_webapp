5b3694ea6ac3a6d222f03b94169bda240898e59d
dahiya18 <harmeetadahiya@gmail.com>
2023-03-20 11:17:27 -0700

    Adding support for traffic light status

--- src/Pages/CobaltController.js
+++ src/Pages/CobaltController.js
@@ -1,5 +1,20 @@
+//import trafficlight from "react-trafficlight";
+import React from "react";
+//import Sidenav from "../Components/SideNav";
+import Light from "../Components/Light/Light"
+//const TrafficLight = trafficlight.default;
+
 export default function CobaltController() {
     return (
-        <div>Cobalt Traffic Light controller</div>
+        <div>
+            <h5>Cobalt Traffic Light controller</h5>
+            <Light/>
+        </div>
     )
 }
+
+//<div><TrafficLight
+//RedOn={true}
+//YellowOn={false}
+//GreenOn={false}
+///></div>
